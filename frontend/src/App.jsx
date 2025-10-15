b415a247eee439fc0829f0eadd98e6c6c6cd2264
Jasmin Shaik <jasminshaik7979@gmail.com>
2025-10-15 14:49:12 +0530

    My Front End Project

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,13 +2,11 @@ import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
 import Services from "./components/Services";
 import PromoSection from "./components/PromoSection";
-//import CaseStudies from "./components/CaseStudies";
 import WorkingProcess from "./components/WorkingProcess";
 import Teams from "./components/Teams"
 import Testimonials from "./components/Testimonials";
 import Contact from "./components/Contact";
 import Footer from "./components/Footer";
-import UserRoute from "./router/UserRoute"
 function App() {
   return (
     <>
@@ -16,13 +14,11 @@ function App() {
       <Hero />
       <Services />
       <PromoSection />
-      {/* <CaseStudies /> */}
       <WorkingProcess />
       <Teams />
       <Testimonials />
       <Contact />
       <Footer />
-      {/* <UserRoute /> */}
     </>
   );
 }
