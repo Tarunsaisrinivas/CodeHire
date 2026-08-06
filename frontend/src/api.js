85bb8b7786ea6a9221f89a0f9443f49c45306903
Tarun Sai Srinivas <tarunsaisrinivas4@gmail.com>
2026-08-06 19:37:47 +0530

    vercel backend kept in frontend

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -9,7 +9,7 @@ export async function fetchJobs(keyword, selectedSites) {
     const controller = new AbortController();
     const timeoutId = setTimeout(() => controller.abort(), 45000); // 45 second timeout
 
-    const res = await fetch("http://localhost:5000/jobs", {
+    const res = await fetch("code-hire-c44s.vercel.app/jobs", {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
