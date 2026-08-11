648d9e6b341b5ca6ce73bc53bd24abb952088123
Tarun Sai Srinivas <tarunsaisrinivas4@gmail.com>
2026-08-11 22:21:54 +0530

    frontend added to cors

--- backend/index.js
+++ backend/index.js
@@ -27,7 +27,7 @@ const allowedOrigins = [
   "http://localhost:5173",
   "http://localhost:3000",
   "http://127.0.0.1:5173",
-
+  "https://code-hire-beryl.vercel.app",
   "https://codehire-oaod.onrender.com",
   "https://code-hire-c44s.vercel.app",
   "https://code-hire-xrhe.vercel.app",
