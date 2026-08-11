b6eaf81aac96d1f983cf297bb222f80658d77ccd
Tarun Sai Srinivas <tarunsaisrinivas4@gmail.com>
2026-08-11 21:54:18 +0530

    updated cors

--- backend/index.js
+++ backend/index.js
@@ -1,42 +1,46 @@
-const express = require("express");
-const cors = require("cors");
-const { Server } = require("socket.io");
 const http = require("http");
 const dotenv = require("dotenv");
-const jobRoute = require("./routes/jobRoute");
-const connectDB = require("./utils/db");
-const roomSocketHandler = require("./sockets/roomSocket");
-const subscribeRoute = require("./routes/subscribeRoute");
-const contactRoute = require("./routes/contactRoute");
+
 dotenv.config();
-const app = express();
+
+const app = require("./app");
+
+const connectDB = require("./utils/db");
+const { Server } = require("socket.io");
+const roomSocketHandler = require("./sockets/roomSocket");
+
 const server = http.createServer(app);
+
+const allowedOrigins = [
+  "http://localhost:3000",
+  "http://localhost:5173",
+  "http://127.0.0.1:5173",
+
+  "https://codehire-oaod.onrender.com",
+  "https://code-hire-c44s.vercel.app",
+  "https://code-hire-xrhe.vercel.app",
+  "https://code-hire-xrhe.vercel.app/jobs",
+];
+
 const io = new Server(server, {
   cors: {
-    origin: [
-      "http://localhost:3000",
-      "http://localhost:5173",
-      "http://127.0.0.1:5173",
-      "http://localhost:5000",
-      "https://codehire-oaod.onrender.com",
-      "https://code-hire-c44s.vercel.app",
-    ],
+    origin: allowedOrigins,
     methods: ["GET", "POST"],
     credentials: true,
   },
-  transports: ["websocket", "polling"], // Add this line
 });
 
-app.use(cors());
-app.use(express.json());
-
-app.use("/jobs", jobRoute);
-app.use("/api/subscribe", subscribeRoute);
-app.use("/contact", contactRoute);
 roomSocketHandler(io);
 
 const port = process.env.PORT || 5000;
-server.listen(port, () => {
-  connectDB();
-  console.log(`Server is running on port ${port}`);
-});
+
+// Only start traditional server locally
+if (process.env.NODE_ENV !== "production") {
+  server.listen(port, async () => {
+    await connectDB();
+
+    console.log(`Server running on port ${port}`);
+  });
+}
+
+module.exports = app;
--- backend/scrapper/naukari.js
+++ backend/scrapper/naukari.js
@@ -1,14 +1,14 @@
-const puppeteer = require("puppeteer");
+const puppeteer = require("puppeteer-core");
+const chromium = require("@sparticuz/chromium");
 
 async function scrapeNaukriJobs(keyword) {
-  const browser = await puppeteer.launch({
-    headless: true,
-    args: [
-      "--no-sandbox",
-      "--disable-setuid-sandbox",
-      "--disable-dev-shm-usage",
-    ],
-  });
+const browser = await puppeteer.launch({
+  args: chromium.args,
+  defaultViewport: chromium.defaultViewport,
+  executablePath: await chromium.executablePath(),
+  headless: chromium.headless,
+  ignoreHTTPSErrors: true,
+});
 
   const page = await browser.newPage();
 
